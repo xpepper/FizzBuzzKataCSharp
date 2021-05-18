58865e94a8f5ecf245c1969efbd9a185ec4c0923
Pietro Di Bello <pierodibello@gmail.com>
2021-05-18 11:07:28 +0200

    remove duplicated tests

--- FizzBuzzKata.Tests/FizzBuzzTest.cs
+++ FizzBuzzKata.Tests/FizzBuzzTest.cs
@@ -6,18 +6,6 @@ namespace FizzBuzzKata.Tests
 {
     public class FizzBuzzTest
     {
-        [Fact]
-        public void Say_1_for_1()
-        {
-            new FizzBuzz().Say(1).Should().Be("1");
-        }
-
-        [Fact]
-        public void Say_2_for_2()
-        {
-            new FizzBuzz().Say(2).Should().Be("2");
-        }
-
         [Theory]
         [InlineData(1, "1")]
         [InlineData(2, "2")]

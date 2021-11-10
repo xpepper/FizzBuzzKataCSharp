6cd0c1d7a3c8c0f07a3def6fb52d7346ad92ee5e
Pietro Di Bello <pierodibello@gmail.com>
2021-11-10 14:58:14 +0100

    add a main method to say all numbers from 1 to 100

--- FizzBuzzKata/FizzBuzz.cs
+++ FizzBuzzKata/FizzBuzz.cs
@@ -1,3 +1,5 @@
+using static System.Linq.Enumerable;
+
 namespace FizzBuzzKata
 {
     public class FizzBuzz
@@ -5,6 +7,16 @@ namespace FizzBuzzKata
         public string Say(int number)
         {
             return number.ToString();
+        }
+
+        public static void Main()
+        {
+            var fizzBuzz = new FizzBuzz();
+            foreach (var number in Range(1, 2))
+            {
+                var say = fizzBuzz.Say(number);
+                System.Console.Out.WriteLine("for {0} we say {1}", number, say);
+            }
         }
     }
 }
